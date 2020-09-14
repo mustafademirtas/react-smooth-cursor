fc5696ae69e484d383038f805cb905404f3f8673
Mustafa <mustafademirtas@gmail.com>
2020-09-15 01:13:27 +0300

    Updates

---
+++ precache-manifest.417cdc75367420a34249657251cc76ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfb55a22d5c536bab035ec11e3b69fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84de5b4e457c61ed5ef1",
+    "url": "./static/css/main.31ba534f.chunk.css"
+  },
+  {
+    "revision": "acec68f72b65728a64f6",
+    "url": "./static/js/2.fcdbc66f.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "./static/js/2.fcdbc66f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84de5b4e457c61ed5ef1",
+    "url": "./static/js/main.eee91b87.chunk.js"
+  },
+  {
+    "revision": "963a977fa00915c4dd4d",
+    "url": "./static/js/runtime-main.ce32a472.js"
+  }
+]);
